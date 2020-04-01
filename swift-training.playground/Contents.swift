294b31a1a1a0d65595b18284cb550553c98503f9
Roman Kisliy <omg.dnepr@gmail.com>
2020-04-01 12:40:57 +0300

    ДЗ 01
    Первые шаги.
    Вывод в консоль.

--- swift-training.playground/Contents.swift
+++ swift-training.playground/Contents.swift
@@ -1,2 +1,11 @@
 
+let firstName = "Roman"
+let lastName = "Kisliy"
+let height = "190"
+let weight = "94,2"
+let age = "24"
 
+print("Имя и Фамилия студента :", firstName, lastName)
+print("Рост :", height)
+print("Вес :", weight)
+print("Возраст :", age)
