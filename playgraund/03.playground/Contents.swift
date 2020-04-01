1de7e43aefc54c6aa87cdb31af0a880b40618421
Roman Kisliy <omg.dnepr@gmail.com>
2020-04-01 18:13:03 +0300

    Д/з к уроку 3

--- playgraund/03.playground/Contents.swift
+++ playgraund/03.playground/Contents.swift
@@ -1,2 +1,17 @@
 
+let (pushUpsMe, pullUpsMe, squatsMe) = (5, 10, 50)
+print("Мои способности - Отжимания:", pullUpsMe, " Подтягивания: ", pushUpsMe, " Присеания: ", squatsMe)
 
+pullUpsMe
+squatsMe
+
+var (pushUpsWife, pullUpsWife, squatsWife) = (5, 10, 50)
+
+pullUpsWife = 2
+pushUpsWife = 3
+squatsWife = 30
+print("Cпособности моей жены - Отжимания:", pullUpsWife, " Подтягивания: ", pushUpsWife, " Присеания: ", squatsWife)
+
+
+let achievements = (pushUpsMe - pushUpsWife, pullUpsMe - pullUpsWife, squatsMe - squatsWife)
+print("Разница в колличестве раз между мной и женой", achievements)
